08759ff4aea1b81509303a28c5e41f8da13b1f88
Angel <angelsanti621@gmail.com>
2025-11-21 23:07:05 -0500

    Fondos.js corregido

--- backend/src/pages/Fondo.js
+++ backend/src/pages/Fondo.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
-const FondoSchema = new mongoose.Schema({
-  url: { type: String, required: true },
-});
+const FondoSchema = new mongoose.Schema(
+  {
+    nombre: { type: String, required: true },
+    url: { type: String, required: true },
+    public_id: { type: String, required: true },
+  },
+  {
+    timestamps: true, // crea createdAt y updatedAt automáticamente
+  }
+);
 
 module.exports = mongoose.model("Fondo", FondoSchema);
